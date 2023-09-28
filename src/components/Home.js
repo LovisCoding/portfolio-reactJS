da18f50a4f4d9d0379faa773dfbc9e69d964dfa7
Arthur Lecomte <arthurlecomtefr@gmail.com>
2023-09-28 15:55:32 +0200

    Portfolio

---
+++ src/components/Home.js
@@ -0,0 +1,35 @@
+import React from "react";
+import '../styles/Home.css';
+import Typed from "react-typed";
+import { AiFillGithub,AiFillLinkedin } from "react-icons/ai";
+
+const Home = () => {
+	return(
+		<>
+		<div className="home">
+			<h1 >Arthur Lecomte</h1>
+				<h2 className="text-slider">
+					<Typed
+						strings={[
+							"Développeur Front-end",
+							"Développeur Back-end",
+							"Développeur Full Stack",
+						]}
+						typeSpeed={80}
+						backDelay={1100}
+						backSpeed={40}
+						loop
+					/>
+				</h2>
+		</div>
+		<div className="socials">
+			<a href="https://github.com/LovisCoding" target="_blank" rel="noopener noreferrer"><AiFillGithub/></a>
+			<a href="https://www.linkedin.com/in/arthur-lecomte-44354923b/" target="_blank" rel="noopener noreferrer"><AiFillLinkedin/></a>
+		</div>
+
+		</>
+
+	)
+
+}
+export default Home;
